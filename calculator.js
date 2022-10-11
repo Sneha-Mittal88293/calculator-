32eab5de4b44b9e62ef9be96cdc535ead8c95ad1
sneha <snehamittal88293@gmail.com>
2022-10-11 16:31:32 +0530

    express js

--- calculator.js
+++ calculator.js
@@ -13,10 +13,20 @@ const express = require("express");
  app.post("/" , function(req , res){
    var num1 = Number(req.body.num1);
    var num2 = Number(req.body.num2);
-
-   res.send(num1 + num2);
+   var result = num1 + num2;
+   res.send("  " + result);
  });
 
+ app.get("/bmicalculator" , function(req , res){
+   res.sendFile(__dirname + "/bmiCalculator.html");
+ });
+
+ app.post("/bmicalculator", function(req , res){
+   var weight = parseFloat(req.body.weight);
+   var height = parseFloat(req.body.height);
+   var bmi = (weight / (height * height));
+   res.send(" " + bmi);
+ });
 
  app.listen(3000 ,function(){
     console.log("Server is running on port 3000");
