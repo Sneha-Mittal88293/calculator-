1982a430a20a708e2ffd3a599e0f3e9c79a1f045
sneha <snehamittal88293@gmail.com>
2022-10-11 15:52:00 +0530

    javascript file

--- calculator.js
+++ calculator.js
@@ -2,8 +2,19 @@
 const express = require("express");
  const app = express();
 
+ const bodyParser = require("body-parser");
+
+ app.use(bodyParser.urlencoded({extended:true}));
+
  app.get("/" , function(req , res){
     res.sendFile(__dirname + "/index.html");
+ });
+
+ app.post("/" , function(req , res){
+   var num1 = Number(req.body.num1);
+   var num2 = Number(req.body.num2);
+
+   res.send(num1 + num2);
  });
 
 
